838675d8561ff7d51dbde5840d3c1135c2646335
nshirley <nickshirley@mac.com>
2024-07-27 13:06:56 -0600

    adds npm run script, baseURL, README, and more auth tests!

---
+++ README.md
@@ -0,0 +1,68 @@
+# lets-tango
+This is the take home project for Tango.us QE role. Tests are written in `playwright` with the default typescript selected.
+
+## Getting Started
+1. Install dependencies `npm install`
+2. Install playwright if you haven't yet, `npx playwright install` (need to double check this command with a new install)
+3. Setup your `.env` file with a copy of `example.env`, populate values for test account!
+4. Run the tests! `npm run test:all`
+
+
+## Known Issues
+There are a few issues I ran into I couldn't prioritize to solve, noted below with my _assumed_ reasons.
+
+- In VSCode, I cannot get debugging working. There's an error of missing module `jest.js`. I'm guessing I have a weird environmental configuration and would need to take a bit of time to dig deeper. Since standard running works I've ignored for now.
+
+## Future Updates
+A few things I would like to also do but don't have time for:
+
+- Add a few more npm scripts for running in headed mode, with specific browsers etc.
+- Clean up references to `process.env` - There's a need to coalesce values which isn't great. There are a few other packages like [config](https://www.npmjs.com/package/config) that would be good to swap out but require more time to setup.
+- Consolidation with parameterized tests. This would be helpful for things like "bad password/email" tests and more. A single test would be reusable for multiple tests.
+- Use setup to handle authentication and bypass it for necessary tests. This would save a lot of time in tests and allow us to focus on functionality under tests, not necessarily the full _end-to-end_ for stability.
+
+### Tests
+These are the tests/scenarios I found with a few minutes of digging through the site and how I would start to prioritize tests. This was originally a comment so, sorry to just drop it in a code block like this!
+
+```
+Tango.us tests outline, these are general tests that would be valuable
+to initially cover. Not all will be touched in this project, but
+these are the tests I can spot that I would _want_ to test given enough
+time.
+
+I'm focusing on Auth and Settings to start. General account management is
+where I like to start as it's basic functionality that shouldn't change often
+but is critical to operations. 
+
+Business functionality like the Team Library, popping the Upgrade Modal,
+"Get the extension" Button would be next priority with groundwork laid in prior tests.
+
+**Tests**
+
+- Auth/login, user can login with username & password
+- Auth/login, user login is rejected for bad email
+- Auth/login, user login is rejected for bad password
+- Auth/login, user can logout
+
+- Settings, search for member
+- Settings, Guests list loads (empty currently)
+- Settings, Invites & requests (empty currently)
+   - Send invite to @mailinator email, shows up in list (this could be flaky, and would require careful management of test data)
+
+- Settings, Profile edit name
+- Settings, Edit password (similar to above, careful management of test data, or would require creating a new account for each test run)
+
+- Settings, Branding requires Pro
+- Settings, Billing shows different tiers
+- Settings, Billing has appropriate tiers (should be parameterized)
+
+- Settings, update workspace name
+- Settings, Leave workspace as only admin (should stop or send to member screen)
+
+--- If time allows
+
+- Team Library, loads no tangos initially
+- Team Library, create folder
+- Team Library, limit access to folder
+- Team Library, access to Pro on click opens Upgrade screen
+```
--- tests/authLogin.spec.ts
+++ tests/authLogin.spec.ts
@@ -1,64 +1,56 @@
 import { test, expect } from '@playwright/test';
 
 
-
-/**
- * Tango.us tests outline, these are general tests that would be valuable
- * to initially cover. Not all will be touched in this project, but
- * these are the tests I can spot that I would _want_ to test given enough
- * time.
- * 
- * I'm focusing on Auth and Settings to start. General account management is
- * where I like to start as it's basic functionality that shouldn't change often
- * but is critical to operations. 
- * 
- * Business functionality like the Team Library, popping the Upgrade Modal,
- * "Get the extension" Button would be next priority with groundwork laid in prior tests.
- * 
- * **Tests**
- * 
- * - Auth/login, user can login with username & password
- * - Auth/login, user login is rejected for bad email
- * - Auth/login, user login is rejected for bad password
- * 
- * - Settings, search for member
- * - Settings, Guests list loads (empty currently)
- * - Settings, Invites & requests (empty currently)
- *    - Send invite to @mailinator email, shows up in list (this could be flaky, and would require careful management of test data)
- * 
- * - Settings, Profile edit name
- * - Settings, Edit password (similar to above, careful management of test data, or would require creating a new account for each test run)
- * 
- * - Settings, Branding requires Pro
- * - Settings, Billing shows different tiers
- * - Settings, Billing has appropriate tiers (should be parameterized)
- * 
- * - Settings, update workspace name
- * - Settings, Leave workspace as only admin (should stop or send to member screen)
- * 
- * --- If time allows
- * 
- * - Team Library, loads no tangos initially
- * - Team Library, create folder
- * - Team Library, limit access to folder
- * - Team Library, access to Pro on click opens Upgrade screen
- * */
-
-
 test('Auth page allows login', async ({ page }) => {
-  await page.goto('https://app.tango.us/sign-in');
+  await page.goto('/');
 
   // input email into email field - `fill` waits for element to be actionable
-  // TODO: 
   await page.getByTestId("auth.signIn.emailInput").fill(process.env.DEFAULT_EMAIL ?? "")
+  await page.getByTestId("auth.signIn.passwordInput").fill(process.env.DEFAULT_PASSWORD ?? "")
+
+  // submit!
+  await page.getByTestId("auth.signIn.submitButton").click()
+
+  // assertion we're successfully logged in (it would be better to look for an element, but this works for now)
+  await expect(page).toHaveTitle("Team Library")
 });
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('Auth page rejects with bad password', async ({ page }) => {
+  await page.goto('/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByTestId("auth.signIn.emailInput").fill(process.env.DEFAULT_EMAIL ?? "")
+  await page.getByTestId("auth.signIn.passwordInput").fill("BAD_PASSWORD_YO")
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+  // submit!
+  await page.getByTestId("auth.signIn.submitButton").click()
+
+  // Check that the alert message is present and button is enabled after failure.
+  // I don't like doing more than one assertion typically, but given familiarity 
+  // of the tool, I want to be extra sure! Another solution could be found later.
+  await expect(page.getByText("We couldn't find that account.")).toBeVisible()
+  await expect(page.getByTestId("auth.signIn.submitButton")).toBeEnabled()
+});
+
+
+test('Auth page rejects with bad username', async ({ page }) => {
+  await page.goto('/');
+  
+  // use a random email to avoid risk of a "real" email
+  const randInt = Math.floor(Math.random() * 10000)
+  const randEmail = `testEmail${randInt}@mailinator.com`
+
+  // fill with bad email and a known good password
+  await page.getByTestId("auth.signIn.emailInput").fill(randEmail)
+  await page.getByTestId("auth.signIn.passwordInput").fill(process.env.DEFAULT_PASSWORD ?? "")
+
+  // submit!
+  await page.getByTestId("auth.signIn.submitButton").click()
+
+  // assert!
+  await expect(page.getByText("We couldn't find that account.")).toBeVisible()
+  await expect(page.getByTestId("auth.signIn.submitButton")).toBeEnabled()
+});
+
+test('User can log out', async({ page }) => {
+  
+})
